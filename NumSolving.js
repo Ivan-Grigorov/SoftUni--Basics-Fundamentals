27830e26529363a994d00f83f17a065b05ed4e15
Ivan E. Grigorov <108576481+Ivan-Grigorov@users.noreply.github.com>
2022-07-02 16:35:22 +0300

    Add files via upload
    Added more exercises

---
+++ NumSolving.js
@@ -0,0 +1,9 @@
+function theatre(number1,precision) {
+       
+      if (precision > 15) {
+         precision = 15;
+      }
+
+      console.log(parseFloat(number1).toFixed);
+
+} theatre(10.5,3)
