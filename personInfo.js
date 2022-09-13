e834bfae316c3e97c116ff7264460b1759df6972
Ivan E. Grigorov <108576481+Ivan-Grigorov@users.noreply.github.com>
2022-09-13 17:45:49 +0300

    Adding some basic codes in JSON.

---
+++ personInfo.js
@@ -0,0 +1,9 @@
+function personInfo(firstName, lastName, age) {
+
+        let obj = {
+                firstName: firstName,
+                lastName,
+        }
+        obj.age = age;
+        return obj;
+}
